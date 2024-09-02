228f8c1038cef6593cd894c6e9648fbff7016ebb
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-02 12:40:57 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Juan Carlos Robledo Morales on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
