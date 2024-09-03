cb21450f4ddeae22cebdaa20ab790fbcf944d43f
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-02 19:19:22 -0600

    Trabajar con fechas

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -15,7 +15,30 @@ struct ContentView: View {
         VStack {
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 2...12, step: 0.25)
             DatePicker("Please enter a date", selection: $wakeUp).labelsHidden()
+            
+            Text(Date.now.formatted(date: .long, time: .shortened))
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date.now.formatted(date: .long, time: .shortened))
+
+
+
         }.padding()
+    }
+    
+    func exampleDater(){
+//        let now = Date.now
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//        let range = now...tomorrow
+
+        //        var components = DateComponents()
+        //        components.hour = 8
+        //        components.minute = 0
+        //        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
