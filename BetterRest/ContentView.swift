df3e4b95d4d6efbb48c34f92c854090ae564d9fe
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-04 18:35:04 -0600

    Day 28 – Project 4, part three

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    @State private var bedtime = ""
+    
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingalert = false
@@ -25,66 +27,78 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             Form{
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
-                    HStack {
+                Section(header: Text("When do you want to wake up?").font(.headline)){
+                    HStack{
                         DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                             .labelsHidden()
                         Text("⏰")
+                    }.frame(maxWidth: .infinity)
+                }
+                Section(header: Text("Desired amount of sleep").font(.headline)){
+                    Stepper("\(sleepAmount.formatted()) hours 🛌", value: $sleepAmount, in: 4...12, step: 0.25).padding(.horizontal)
+                }
+                Section(header: Text("Daily coffee intake").font(.headline)) {
+                    HStack {
+                        Text("☕️")
+                            .font(.largeTitle)
+                            .frame(maxWidth: .infinity ,alignment: .trailing)
+                        Picker("Number of cups", selection: $coffeeAmount) {
+                            ForEach(1..<21) { number in
+                                Text("\(number) \(number == 1 ? "cup" : "cups")")
+                            }
+                        }
+                        .frame(alignment: .leading)
+                        .pickerStyle(WheelPickerStyle())
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
                     }
                 }
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
-                    
-                    Stepper("\(sleepAmount.formatted()) hours 🛌", value: $sleepAmount, in: 4...12, step: 0.25)
-                }
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    
-//                    Stepper(coffeeAmount == 1 ? "\(coffeeAmount) cup ☕️" : coffeeAmount == 2 ? "\(coffeeAmount) cups ☕️☕️" : "\(coffeeAmount) cups ☕️☕️☕️", value: $coffeeAmount, in: 1...20)
-                    Stepper("^[\(coffeeAmount) cup](inflect: true) ☕️", value: $coffeeAmount, in: 1...20)
-                }
-            }.navigationTitle("BetterRest")
-                .toolbar {
-                    Button("Calculate", action: calculateBedtime)
-                }
-                .alert(alertTitle, isPresented: $showingalert){
-                    Button("ok") {}
-                } message: {
-                    Text(alertMessage)
-                }
+
+                Section(header: Text("Your ideal bedtime is...").font(.headline)) {
+                                Text(bedtime)
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.indigo)
+                                    
+                                    .padding()
+                }.frame(maxWidth: .infinity, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            .navigationTitle("BetterRest")
+            .onChange(of: wakeUp) {
+                calculateBedtime()
+            }
+            .onChange(of: sleepAmount) {
+                calculateBedtime()
+            }
+            .onChange(of: coffeeAmount) {
+                calculateBedtime()
+            }
         }
     }
     func calculateBedtime() {
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
-            let sleeptime = wakeUp - prediction.actualSleep
-            
-            alertTitle = "Your ideal vedtime is..."
-            alertMessage = sleeptime.formatted(date: . omitted, time: .shortened)
-            
-        } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating yout bedtime."
-        }
-        
-        showingalert = true
-    }
-}
+            do {
+                let config = MLModelConfiguration()
+                let model = try SleepCalculator(configuration: config)
+                let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+                let hour = (components.hour ?? 0) * 60 * 60
+                let minute = (components.minute ?? 0) * 60
 
+                let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+                
+                let sleepTime = wakeUp - prediction.actualSleep
+                bedtime = sleepTime.formatted(date: .omitted, time: .shortened)
+                
+            } catch {
+                bedtime = "Error calculating bedtime."
+            }
+        }
+    }
 #Preview {
     ContentView()
 }
